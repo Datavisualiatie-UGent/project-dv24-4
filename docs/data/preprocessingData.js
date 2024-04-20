38475f25ab3b47598882957cefdee1b7e153b9d4
jobyl <jonas.byle@ugent.be>
2024-04-20 13:53:26 +0200

    load from lokal data

--- docs/data/preprocessingData.js
+++ docs/data/preprocessingData.js
@@ -25,14 +25,32 @@ async function text(url) {
   return response.text();
 }
 
+// TODO terug naar fetch
+async function readFile(filePath) {
+  try {
+      return await fs.readFile(filePath, 'utf-8');
+  } catch (error) {
+    throw new Error(`readFile failed: ${error}`);
+  }
+}
+
 let year = startDate.year;
 let month = startDate.month;
 
 // csv header
-let dataCsv = "siteID,richting,type,van,tot,aantal\n";
+let dataCsv = ["siteID,richting,type,van,tot,aantal"];
 
 while (year < endDate.year || (year === endDate.year && month <= endDate.month)) {
-    dataCsv += await text(`https://opendata.apps.mow.vlaanderen.be/fietstellingen/data-${year}-${String(month).padStart(2, '0')}.csv`);
+    // TODO terug naar fetch
+    // const dataSet = await text(`https://opendata.apps.mow.vlaanderen.be/fietstellingen/data-${year}-${String(month).padStart(2, '0')}.csv`);
+    const dataSet = await readFile(`./dataset/data/data-${year}-${String(month).padStart(2, '0')}.csv`);
+
+    console.log("Loaded data for " + year + "-" + String(month).padStart(2, '0'));
+
+    const rows = dataSet.split('\n');
+    for (let row of rows) {
+        dataCsv.push(row);
+    }
     month++;
     if (month > 12) {
         year++;
@@ -40,10 +58,34 @@ while (year < endDate.year || (year === endDate.year && month <= endDate.month))
     }
 }
 
-const data = csvParse(dataCsv, (d) => ({
+console.log(dataCsv.length + " rows loaded");
+
+function filterData(data){
+    // remove zero counts
+    if (data[5] === "0"){
+        return false;
+    }
+
+    return data[2] === "FIETSERS";
+
+}
+
+console.log(dataCsv[0]);
+// filter data
+dataCsv = dataCsv.filter((row, index) => {
+    const values = row.split(',');
+    return filterData(values) || index === 0;
+});
+console.log(dataCsv[0]);
+// map data
+//dataCsv = dataCsv.map(row => row.replace(/,FIETSERS,/g, ',,'));
+
+console.log(dataCsv.length + " rows after filtering");
+
+const data = csvParse(dataCsv.join('\n'), (d) => ({
     siteID: d.siteID,
     richting: d.richting,
-    type: d.type,
+    // type: d.type,
     van: d.van,
     tot: d.tot,
     aantal: d.aantal

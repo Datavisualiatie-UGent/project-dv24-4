bfd10370f63a14aa2bd5141f1f1dc3bb8214fa89
jobyl <jonas.byle@ugent.be>
2024-05-02 15:25:50 +0200

    start data loader for acc mean

---
+++ docs/data/cumulativeMeanPerMonth.csv.js
@@ -0,0 +1,22 @@
+import fs from "node:fs/promises"
+
+const data = (await fs.readFile("./tellingen.csv", "utf8")).split("\n")
+
+const header = data[0].split(",")
+
+const tellingen = data.slice(1, -1);
+
+console.log(header.join(","))
+
+// get all years in different arrays
+const years = tellingen.reduce((acc, telling) => {
+    const year = telling.split(",")[2].split("-")[0]
+    if (!acc[year]){
+        acc[year] = []
+    }
+    acc[year].push(telling)
+    return acc
+}, {})
+
+console.log(years)
+

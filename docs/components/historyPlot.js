3154f449f053065a99cfc6fe5a62f81f7b5bf37c
jobyl <jonas.byle@ugent.be>
2024-05-10 12:14:54 +0200

    code cleanup

--- docs/components/historyPlot.js
+++ docs/components/historyPlot.js
@@ -2,23 +2,6 @@ import * as Plot from "npm:@observablehq/plot";
 import * as d3 from "npm:d3";
 
 /**
- * This function calculates the number of months between two dates.
- * @param date1
- * @param date2
- * @returns {number}
- */
-export function calculateMonthsBetween(date1, date2) {
-    const startDate = new Date(date1);
-    const endDate = new Date(date2);
-
-    const years = endDate.getFullYear() - startDate.getFullYear();
-    const months = endDate.getMonth() - startDate.getMonth();
-
-    return years * 12 + months;
-}
-
-
-/**
  * This function calculates the month when adding an index to a start date.
  * @param startDate
  * @param index
@@ -34,21 +17,30 @@ function getMonth(startDate, index) {
     return monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear();
 }
 
+/**
+ * This function plots the normalized data.
+ * @param normalizedSiteCumulativeCountsGemeente object containing per gemeente the normalized the cumulative counts normalized by month 0
+ * @param startDate the start date of the data
+ * @param gemeenteActiveSince the date when the gemeente started
+ * @param totalMothsCount the total number of months
+ * @param width the width of the plot
+ * @returns {*}
+ */
 export function plotNormalizedData(normalizedSiteCumulativeCountsGemeente, startDate, gemeenteActiveSince, totalMothsCount, {width} = {}) {
 
     const lines = Array.from(Object.entries(normalizedSiteCumulativeCountsGemeente)).map(([gemeente, counts], i) => {
-    const data = counts.map((value, timeslot) => ({
-        timeslot,
-        value,
-        gemeente
-    })).filter((value, timeslot) => timeslot >= gemeenteActiveSince[gemeente])
+        const data = counts.map((value, timeslot) => ({
+            timeslot,
+            value,
+            gemeente
+        })).filter((value, timeslot) => timeslot >= gemeenteActiveSince[gemeente])
 
-    return Plot.lineY(data, {
-        x: "timeslot",
-        y: "value",
-        stroke: "gemeente",
+        return Plot.lineY(data, {
+            x: "timeslot",
+            y: "value",
+            stroke: "gemeente",
+        });
     });
-});
 
     const minY = d3.min(lines, line => d3.min(line.data, d => d.value));
     const maxY = d3.max(lines, line => d3.max(line.data, d => d.value));
--- docs/data/cumulativeMeanPerMonth.json.js
+++ docs/data/cumulativeMeanPerMonth.json.js
@@ -3,17 +3,13 @@ import {sites} from "../components/sitesUtiles.js";
 import * as d3 from "d3";
 import {getResult,} from "../components/historyUtils.js";
 
+
 //const data = (await fs.readFile("./tellingen.csv", "utf8")).split("\n")
 const data = (await fs.readFile("./docs/data/tellingen.csv", "utf8")).split("\n")
 
 
 const header = data[0].split(",")
-
-
-
 const tellingen = data.slice(1, -1);
-
-//console.log(header.join(","))
 
 // get all years in different arrays
 const years = tellingen.reduce((acc, telling) => {
@@ -26,16 +22,17 @@ const years = tellingen.reduce((acc, telling) => {
 }, {})
 
 
+// group the sites by gemeente
 const siteIDs = new Map();
+// save the names of the gemeentes
 let names = [];
-
 for (let [key, values] of d3.group(sites, d => d.gemeente)) {
   const ids = values.map(d => d.siteID);
   siteIDs.set(key, ids);
   names.push(key);
 }
 
-
+// create the resultJSON of all years
 const resultJSON = {};
 for (let [year, data] of Object.entries(years)) {
     resultJSON[year] = getResult(header, data, siteIDs, sites);

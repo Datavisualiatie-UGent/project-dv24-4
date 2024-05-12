700ab30a4ffb55cf53fe36d00c331fbfcb94bb5a
Michiel Lachaert <michiellachaert@outlook.com>
2024-05-13 00:36:59 +0200

    fix y-labels drukte graph

--- docs/components/estimatedOverview.js
+++ docs/components/estimatedOverview.js
@@ -8,13 +8,13 @@ export function estimatedOverview(data, k, width, showY= true) {
             label: "datum"
         },
         y: {
-            axis: showY ? true : null,
             label: "drukte",
             grid: true
         },
 
         marks: [
-            Plot.ruleY([0], {stroke: "black", fillOpacity: 0, strokeOpacity: 0  }),
+            showY ? Plot.axisY() : Plot.axisY({ticks:1, textStroke:"black", textStrokeOpacity:1, textStrokeWidth: 1}),
+            Plot.ruleY([0], {stroke: "red"}),
             Plot.lineY(data, Plot.windowY({k: k, reduce: "mean"}, {x: (d) => new Date(d.datum), y: "aantal", stroke: "grey", curve:"basis"})),
         ]
     })
